37ac541e32024a22f45cf5b74fb1ab68afda3437
gohomeandsleep <kangth2103@gmail.com>
2024-03-20 20:40:43 +0900

    solving Simultaneous linear equations with numpy

--- step12_브루트 포스/12_3_cf.py
+++ step12_브루트 포스/12_3_cf.py
@@ -2,13 +2,14 @@ import numpy as np
 
 numlist = list(map(int, input().split()))
 
-# Define the coefficients matrix (A) and the constants vector (B)
+#1행부터 차례로 작성
 A = np.array([[numlist[0], numlist[1]], [numlist[3], numlist[4]]])
+#상수 행렬 작성
 B = np.array([numlist[2], numlist[5]])
 
-# Solve the system of linear equations
+# 연립일차식 풀이
 X = np.linalg.solve(A, B)
-
+#리스트 형태로 바꾸어서 표현
 X = X.tolist()
 
 print(round(X[0]), round(X[1]))
