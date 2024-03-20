04b61b86c8557a4552a88f4165d183af3f61a050
gohomeandsleep <kangth2103@gmail.com>
2024-03-20 21:07:45 +0900

    solving matrix using brute force

--- step12_브루트 포스/12_3.py
+++ step12_브루트 포스/12_3.py
@@ -4,16 +4,8 @@ input = sys.stdin.readline
 a,b,c,d,e,f = map(int, input().split())
 arrx = []
 arry = []
-for i in range(1999):
-  for j in range(1999):
-    if a * (i-999) + b * (j-999) == c:
-      arrx.append(i-999)
-      arry.append(j-999)
+for i in range(-999, 1000):
+  for j in range(-999, 1000):
+    if a * i + b * j == c and d * i + e * j == f:
+      print(i, j)
 
-ans = 0
-i = 0
-while ans == 0:
-  if d * arrx[i] + e * arry[i] == f:
-    print(arrx[i], arry[i])
-    ans = 1
-  i = i + 1
