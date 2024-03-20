a8a18b8771c536c524311fbc887d8800b64eedb6
gohomeandsleep <kangth2103@gmail.com>
2024-03-20 19:12:03 +0900

    blackjack

--- step12_브루트 포스/12_1.py
+++ step12_브루트 포스/12_1.py
@@ -3,9 +3,9 @@ from math import comb
 
 n, m = map(int, input().split())
 num_list = list(map(int, input().split()))
-
+#세 숫자를 더하기 위해 조합 리스트 저장
 temp_list = list(combinations(num_list, 3))
-
+#모든 경우에 걸쳐서 가장 크면서 m을 넘지 않는 조합 찾기
 max_sum = 0
 for combination in temp_list:
     part_sum = sum(combination)
