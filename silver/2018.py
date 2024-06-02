401cfac79dbc50cbf039da7da2d061cc549ad466
gohomeandsleep <kangth2103@gmail.com>
2024-06-02 10:18:25 +0900

    sum of array

---
+++ silver/2018.py
@@ -0,0 +1,15 @@
+n = int(input())
+lst = [i+1 for i in range(n)]
+
+cnt = 0
+for i in range(n): #시작점
+    current_sum = 0
+    for j in range(i, n): #개수
+        current_sum += lst[j]
+        if current_sum == n:
+            cnt += 1
+            #print(lst[i:j+1])
+        elif current_sum > n:
+            break
+
+print(cnt)
