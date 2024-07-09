750a0f4a2e8ade5706c60d6014f44b966bba2e28
gohomeandsleep <kangth2103@gmail.com>
2024-07-09 16:24:03 +0900

    useful codes

--- useful_code.py
+++ useful_code.py
@@ -3,14 +3,12 @@ import sys
 sys.stdin.readline().rstrip()
 
 #최대공약수 구하기(gcd)
-
 def gcd(a, b):
     while b != 0:
         a, b = b, a % b
     return a
 
 #피보나치 수열
-
 def fibonacci(n):
     if n <= 1:
         return n
